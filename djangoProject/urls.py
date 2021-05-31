491849007502a52fec39d9d1add985b884887ba3
Caspar <casparhendrik12@gmail.com>
2021-05-31 14:11:13 +0200

    Add settings

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .connector import send_message, make_as_read
 
@@ -25,3 +27,6 @@ urlpatterns = [
     path('ajax/send-message/', send_message, name='send_message'),
     path('ajax/make-as-read/', make_as_read, name='make_as_read')
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
