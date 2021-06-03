0d8e09e6c50025cb9dcb750be2f0ff17aa379b36
Caspar <casparhendrik12@gmail.com>
2021-06-03 16:10:49 +0200

    Retrieve rooms from firestore

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=40)
+    logo = models.ImageField(upload_to='rooms', default='no_picture.png')
+    room = models.CharField(max_length=50)
+    progression = models.IntegerField()
+    status = models.CharField(max_length=40)
--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
 
+from rooms.models import Room
+import firebase_admin
+import google.cloud.exceptions
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate('')
+default_app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
 # Create your views here.
 
 def room_view(request):
-    databaseCall = 'Tim yhbufgdhbrefh'
-    return render(request, 'rooms/main.html', {'T': databaseCall})
+    room = request.GET.get('room', '')
+    print(room)
+    if room != '':
+        room = get_room(room)
+        databaseCall = room
+        return render(request, 'rooms/main.html', {'T': databaseCall})
+    else:
+        databaseCall = 'No room selected'
+        return render(request, 'rooms/main.html', {'T': databaseCall})
+
+def get_room(room):
+    rooms_ref = db.collection(u'Rooms')
+    rooms = rooms_ref.where(u'room', u'==', room).get()
+
+    # loops through all rooms, count of rooms should be one since each room is unique.
+    roomObj = {
+
+    }
+    for room in rooms:
+        roomObj = Room(**room.to_dict())
+
+    return roomObj
